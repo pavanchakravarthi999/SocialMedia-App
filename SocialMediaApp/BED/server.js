8a4c08879d0f2933accfbcfb21500bbf383432c0
pavanchakravarthi999 <55144583+pavanchakravarthi999@users.noreply.github.com>
2024-07-12 09:53:46 +0530

    Add files via upload

---
+++ SocialMediaApp/BED/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/validationRoutes");
+const postRoutes = require("./routes/postRoutes");
+
+const PORT = 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mongoose.connect("mongodb+srv://Pavan:Pavan123@cluster0.oq8ruoi.mongodb.net/")
+
+app.use('/socialMedia/user',userRoutes);
+app.use('/socialMedia/authToken', authRoutes);
+app.use('/socialMedia/posts', postRoutes);
+
+app.listen(PORT,()=>console.log(`Server is running at port: ${PORT}`))
